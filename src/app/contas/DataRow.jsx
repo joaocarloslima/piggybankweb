2c553c16493439cd2d711eedb75c32e061239863
Joao <joaocarloslima@me.com>
2023-08-21 20:45:13 -0300

    fetch

---
+++ src/app/contas/DataRow.jsx
@@ -0,0 +1,20 @@
+import DropMenu from "@/components/DropMenu";
+import { CreditCardIcon } from "@heroicons/react/24/outline";
+
+export default function DataRow({conta}) {
+    const {nome} = conta
+    return (
+        <div id="data-row" className="group/row flex items-center justify-between hover:bg-slate-800 p-2 rounded cursor-pointer">
+            <div className="flex gap-1">
+                <CreditCardIcon className="h-6 w-6" />
+                <span>{nome}</span>
+            </div>
+            <div className="flex items-center">
+                <span>R$ 100.00</span>
+                <span className="invisible group-hover/row:visible" >
+                    <DropMenu />    
+                </span>
+            </div>
+        </div>
+    )
+}
--- src/app/contas/page.js
+++ src/app/contas/page.js
@@ -1,12 +1,26 @@
 import NavBar from "@/components/NavBar";
+import DataRow from "./DataRow";
 
-export default function Home() {
+async function getContas(){
+  const url = "http://localhost:8080/api/contas"
+  const resp = await fetch(url, { next: { revalidate: 0 } })
+  if (!resp.ok) throw new Error("Não pode carregar os dados")
+  return resp.json()
+}
+
+export default async function Home() {
+  const data = await getContas()
+
   return (
     <>
       <NavBar active={"contas"} />
 
       <main className="bg-slate-900 m-20 p-12 rounded-xl">
-        <h2>Contas</h2>
+        <h2 className="text-2xl font-bold">Contas</h2>
+
+        <div id="data" className="text-slate-300 m-1">
+          {data.map(conta => <DataRow conta={conta} /> )}
+        </div>
       </main>
     </>
 
